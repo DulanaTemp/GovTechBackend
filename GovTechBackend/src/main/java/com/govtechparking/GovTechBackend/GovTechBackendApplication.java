726643a2e9dbce669e36965b0a2a0b29aabf3a4d
Dulana Ratnayake <DULANAR@mobitel.lk>
2026-08-19 14:24:11 +0530

    Initial Commit

---
+++ GovTechBackend/src/main/java/com/govtechparking/GovTechBackend/GovTechBackendApplication.java
@@ -0,0 +1,13 @@
+package com.govtechparking.GovTechBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovTechBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovTechBackendApplication.class, args);
+	}
+
+}
---
+++ GovTechBackend/src/test/java/com/govtechparking/GovTechBackend/GovTechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govtechparking.GovTechBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovTechBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
